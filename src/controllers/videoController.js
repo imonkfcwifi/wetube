3715a5ea331951ca3444725299a6f742370d1115
Roh <110550852+imonkfcwifi@users.noreply.github.com>
2022-10-19 23:18:52 +0900

    404 page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,11 +10,18 @@ export const homepageVideos = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await dideo.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found..!" });
+    }
     return res.render("watch", { pageTitle: video.title, video });
 }
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Edit` });
+    const video = await dideo.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video Not Found..!" });
+    }
+    return res.render("edit", { pageTitle: `Edit : ${video.title}`, video });
 }
 
 export const postEdit = (req, res) => {
